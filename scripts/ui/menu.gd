3383056f013dec93a9c1b68a3ef942f7aaacebe3
anaclarasff <anaclarafcld25@gmail.com>
2026-09-08 12:00:03 -0300

    Criação do menu e edição dele na main
    Menu com os botões adicionado na main, para teste de jogo e ver se os botões estão funcionando na execução

--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -19,5 +19,6 @@ func _on_button_2_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_button_3_pressed() -> void:
-	pass # Replace with function body.
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/modes/order_mode.tscn")
+	
